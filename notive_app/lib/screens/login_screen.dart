bdb3b73f9dc64c76d18f35cb650a55510e051906
slanci <incisila@gmail.com>
2020-02-16 20:45:01 +0300

    backend connected

--- notive_app/lib/screens/login_screen.dart
+++ notive_app/lib/screens/login_screen.dart
@@ -1,3 +1,4 @@
+<<<<<<< Updated upstream
 import 'package:flutter/material.dart';
 import 'package:notive_app/components/rounded_button.dart';
 import 'package:notive_app/screens/constants.dart';
@@ -127,3 +128,135 @@ class _LoginScreenState extends State<LoginScreen> {
     );
   }
 }
+=======
+import 'package:flutter/material.dart';
+import 'package:notive_app/components/rounded_button.dart';
+import 'package:notive_app/screens/constants.dart';
+import 'dashboard_screen.dart';
+import 'package:modal_progress_hud/modal_progress_hud.dart';
+import 'package:http/http.dart';
+import 'package:notive_app/util/request.dart';
+
+class LoginScreen extends StatefulWidget {
+  static const String id = 'login_screen';
+  @override
+  _LoginScreenState createState() => _LoginScreenState();
+}
+
+class _LoginScreenState extends State<LoginScreen> {
+  bool showSpinner = false;
+  String email;
+  String password;
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text('LogIn'),
+      ),
+      //backgroundColor: Colors.black,
+      body: ModalProgressHUD(
+        inAsyncCall: showSpinner,
+        child: Padding(
+          padding: EdgeInsets.symmetric(horizontal: 24.0),
+          child: Column(
+            mainAxisAlignment: MainAxisAlignment.center,
+            crossAxisAlignment: CrossAxisAlignment.stretch,
+            children: <Widget>[
+              Flexible(
+                child: Hero(
+                  tag: 'logo',
+                  child: Container(
+                    height: 200.0,
+                    child: Image.asset('images/logo.png'),
+                  ),
+                ),
+              ),
+              SizedBox(
+                height: 48.0,
+              ),
+              TextField(
+                keyboardType: TextInputType.emailAddress,
+                textAlign: TextAlign.center,
+                onChanged: (value) {
+                  email = value;
+                },
+                decoration: kTextFieldDecorationLog.copyWith(
+                    hintText: 'Enter your email'),
+              ),
+              SizedBox(
+                height: 8.0,
+              ),
+              TextField(
+                obscureText: true,
+                textAlign: TextAlign.center,
+                onChanged: (value) {
+                  password = value;
+                },
+                decoration: kTextFieldDecorationLog.copyWith(
+                    hintText: 'Enter your password'),
+              ),
+              SizedBox(
+                height: 24.0,
+              ),
+              RoundedButton(
+                title: 'Log In',
+                colour: kLightBlueColor,
+                onPressed: () async {
+                  //print(email);
+                  // String data = "{'password': '" + password + "', 'email': '" + email + "'}";
+                  Map<String, dynamic> data = {
+                    'password': password,
+                    'email': email
+                  };
+
+                  int userID;
+                  String user_email, name, surname;
+                  List<dynamic> response = await loginUser(data);
+                  int status = response[0];
+                  String msg = response[1];
+                  if(status == 200){
+                    Map<String, dynamic> data = response[2];
+                    user_email = data['email'];
+                    name = data['name'];
+                    surname = data['surname'];
+                  } else {
+                    // Error message with msg variable message.
+                  }
+
+                  setState(() {
+                    showSpinner = true;
+                    print(response);
+                  });
+//                  try {
+//                    final user = await _auth.signInWithEmailAndPassword(
+//                        email: email, password: password);
+//                    if (user != null) {
+//                      
+//                    }
+//
+//                    setState(() {
+//                      showSpinner = false;
+//                    });
+//                  } catch (e) {
+//                    print(e);
+//                  }
+                },
+              ),
+              Text(
+                'Forgot password?',
+                textAlign: TextAlign.center,
+                overflow: TextOverflow.ellipsis,
+                style: TextStyle(
+                  fontWeight: FontWeight.w400,
+                  fontStyle: FontStyle.italic,
+                ),
+              ),
+            ],
+          ),
+        ),
+      ),
+    );
+  }
+}
+>>>>>>> Stashed changes
--- notive_app/lib/util/request.dart
+++ notive_app/lib/util/request.dart
@@ -1,65 +1,99 @@
 import 'dart:async';
 import 'dart:convert';
+//import 'dart:io';
 
 import 'package:http/http.dart' as http;
-
 String apiUrl = "http://139.179.206.249/"; //temp
 
-Future<List<dynamic>> sendRequest(
-    String url, Map<String, dynamic> reqBody, String method) async {
-  if (method == "POST") {
-    final response = await http.post(apiUrl + url,
-        headers: {
-          'Content-type': 'application/json',
-          'Accept': 'application/json',
-          'Authorization':
-              'Bearer P342A8HtvavVuSDNKV2fMd9TZctxVLs8Cw2I2nfi6HlMIUFPc51MV66zYdwOPUno'
-        },
-        body: json.encode(reqBody));
+Future <List<dynamic>> sendRequest(String url, Map<String,dynamic> reqBody, String method) async {
+  if (method == "POST"){
+    final response = await http.post(
+      apiUrl + url,
+      headers: {'Content-type':'application/json',
+      'Accept':'application/json',
+      'Authorization': 'Bearer P342A8HtvavVuSDNKV2fMd9TZctxVLs8Cw2I2nfi6HlMIUFPc51MV66zYdwOPUno'},
+      body: json.encode(reqBody)
+    );
     final responseJson = json.decode(response.body);
-    return [response.statusCode, responseJson];
-  }
+    return [response.statusCode,responseJson];
+  } 
 
-  if (method == "GET") {
+  if (method == "GET"){
     final response = await http.get(
       apiUrl + url,
-      headers: {
-        'Content-type': 'application/json',
-        'Accept': 'application/json',
-        'Authorization':
-            'Bearer P342A8HtvavVuSDNKV2fMd9TZctxVLs8Cw2I2nfi6HlMIUFPc51MV66zYdwOPUno'
-      },
+      headers: {'Content-type':'application/json',
+      'Accept':'application/json',
+      'Authorization': 'Bearer P342A8HtvavVuSDNKV2fMd9TZctxVLs8Cw2I2nfi6HlMIUFPc51MV66zYdwOPUno'},
     );
     final responseJson = json.decode(response.body);
     return responseJson;
   }
 
-  if (method == "DELETE") {
+  if (method == "DELETE"){
     final response = await http.delete(
       apiUrl + url,
-      headers: {
-        'Content-type': 'application/json',
-        'Accept': 'application/json',
-        'Authorization':
-            'Bearer P342A8HtvavVuSDNKV2fMd9TZctxVLs8Cw2I2nfi6HlMIUFPc51MV66zYdwOPUno'
-      },
+      headers: {'Content-type':'application/json',
+      'Accept':'application/json',
+      'Authorization': 'Bearer P342A8HtvavVuSDNKV2fMd9TZctxVLs8Cw2I2nfi6HlMIUFPc51MV66zYdwOPUno'},
     );
     final responseJson = json.decode(response.body);
     return responseJson;
   }
 
-  if (method == "PUT") {
-    final response = await http.put(apiUrl + url,
-        headers: {
-          'Content-type': 'application/json',
-          'Accept': 'application/json',
-          'Authorization':
-              'Bearer P342A8HtvavVuSDNKV2fMd9TZctxVLs8Cw2I2nfi6HlMIUFPc51MV66zYdwOPUno'
-        },
-        body: reqBody);
+  if (method == "PUT"){
+    final response = await http.put(
+      apiUrl + url,
+      headers: {'Content-type':'application/json',
+      'Accept':'application/json',
+      'Authorization': 'Bearer P342A8HtvavVuSDNKV2fMd9TZctxVLs8Cw2I2nfi6HlMIUFPc51MV66zYdwOPUno'},
+      body: reqBody
+    );
     final responseJson = json.decode(response.body);
     return responseJson;
   }
 
-  //TODO  check return time (if none of conditions is satisfied
+   //??
+}
+
+Future<List<dynamic>> loginUser(Map<String, dynamic> data) async {
+  List<dynamic> response = await sendRequest('auth/login', data, 'POST');
+    Map<String, dynamic> result = response[1]; // Response from API.
+    if (response[0] != 200) { // Status code from API.
+      return [response[0], result['message']]; // Return error message from API.
+    } else {
+      String message = result['message'];
+      int userID = result['data']['user']['id'];
+      String email = result['data']['user']['email'];
+      String name = result['data']['user']['name'];
+      String surname = result['data']['user']['surname'];
+      Map<String, dynamic> userDetails = {
+        'user_id': userID,
+        'email': email,
+        'name': name,
+        'surname': surname
+      };
+      return [response[0], message, userDetails];
+    }
+}
+
+Future<List<dynamic>> signupUser (Map<String,dynamic> data) async{
+  List<dynamic> response = await sendRequest('auth/register', data,'POST');
+  Map<String, dynamic> result = response[1]; //Response from API.
+  if (response[0] != 200){ //Status code from API.
+    return [response[0], result['message']]; //Return error message from API.
+  } else {
+    String message = result['message'];
+    int userID = result['data']['lists']['id'];
+    //String 
+  }
+}
+
+Future<List<dynamic>> getUserLists (Map<String, dynamic> data) async{
+  List<dynamic> response = await sendRequest('list', data, 'GET');
+  Map<String, dynamic> result = response[1];
+  if(response[0] != 200){
+    return [response[0], result['message']];
+  } else{
+    String message = result['message'];
+  }
 }
